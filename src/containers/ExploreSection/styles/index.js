2f265216480ba4ebfdd3c65c84177ef7342beedc
Kiah Hewitt <kiah.hewitt@capco.com>
2022-08-03 16:28:00 +0100

    responsive design for explore section complete

--- src/containers/ExploreSection/styles/index.js
+++ src/containers/ExploreSection/styles/index.js
@@ -2,30 +2,35 @@ import styled from "styled-components/macro";
 
 const Wrapper = styled.div`
   .explore-container {
-      height: 600px;
-      background-color: #000;
-      padding: 0 24px;
-      
+    display: grid;
+    background-color: #000;
+    padding: 50px 24px;
   }
 
   .explore-title {
     margin:0;
-    padding: 50px 0 50px 0;
+    padding: 50px 0;
     color: #fff;
     font-size: 48px;
     font-family: 'Montserrat';
     font-style: normal;
+    @media screen and (max-width: 768px) {
+      text-align: center;
+    }
   }
 
   .explore-content {
-    width: 100%;
-    // padding: 20px;
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-gap: 20px;
+
+    @media screen and (max-width: 768px) {
+      grid-template-columns: 1fr;
+    }
   }
 
   .explore-item {
-    width: 45%;
-    float: left;
-    padding: 20px;
+    grid-column: span 1;
   }
 
 `;
