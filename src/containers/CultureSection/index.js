c3282d2e588a374c8ed5f01d1c5f530be06c5cc8
Kiah Hewitt <kiah.hewitt@capco.com>
2022-08-08 12:47:59 +0100

    added culture responsiveness

--- src/containers/CultureSection/index.js
+++ src/containers/CultureSection/index.js
@@ -8,14 +8,62 @@ function CultureSection() {
         <p className='culture-title'>You’re in good company</p>
         <p className='culture-subtitle'>Meet some of the team that help to create our team culture and ...</p>
         <div className='culture-employees'>
-            <div className='culture-employee'>1</div>
-            <div className='culture-employee'>3</div>
-            <div className='culture-employee'>2</div>
-            <div className='culture-employee'>4</div>
-            <div className='culture-employee'>5</div>
-            <div className='culture-employee'>6</div>
-            <div className='culture-employee'>7</div>
-            <div className='culture-employee'>8</div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
+            <div className='culture-employee'>
+              <img 
+                className='employee-image'
+                src={require("../../images/default-team-member.jpg")}
+                alt="team member" 
+              />
+            </div>
         </div>
       </div>
     </Wrapper>
