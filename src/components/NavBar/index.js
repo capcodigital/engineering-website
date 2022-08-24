1d15637441d9fb287869880a08ea68162a505f98
Kiah Hewitt <kiah.hewitt@capco.com>
2022-08-24 10:34:41 +0100

    MUI responsive navbar complete

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -6,40 +6,128 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import Logo from '../../images/capco-logo.png';
-import Wrapper from './styles';
+import { Wrapper, StyledDrawer, StyledList, StyledListItem } from './styles';
+import PropTypes from 'prop-types';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const darkTheme = createTheme({
-    palette: {
-      mode: 'dark',
-      primary: {
-        main: '#1976d2',
-      },
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#ffffff',
     },
-  });
+  },
+});
 
-function NavBar() {
+const drawerWidth = 240;
+const navItems = ['Home', 'About', 'Contact'];
+
+function NavBar(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <List className='drawer-list'>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
   return (
     <Wrapper>
-        <ThemeProvider theme={darkTheme}>
-            <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    <div className='logo-container'>
-                        <img className="capco-logo" src={Logo} alt="logo"/>
-                        <span className="logo-text">Engineering</span>
-                    </div>
-                </Typography>
-                    <Button color="inherit" className="nav-links">Capabilities</Button>
-                    <Button color="inherit" className="nav-links">Team</Button>
-                    <Button color="inherit" className="nav-links">Career</Button>
-                    <Button color="inherit" className="nav-links">Articles</Button>
-                    <Button color="inherit" className="nav-links">Capco.com</Button>
-                </Toolbar>
-            </AppBar>
-            </Box>
-        </ThemeProvider>
+    <Box sx={{ display: 'flex' }}>
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: 'block' }}
+          >
+            <div className="logo-container">
+              <img className="capco-logo" src={Logo} alt="logo"/>
+              <span className="logo-text">Engineering</span>
+            </div>
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <StyledDrawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+          className=""
+        >
+          {drawer}
+        </StyledDrawer>
+      </Box>
+    </Box>
     </Wrapper>
-  )
+  );
 }
 export default NavBar;
+
+// <Wrapper>
+    //     <ThemeProvider theme={darkTheme}>
+    //         <Box sx={{ flexGrow: 1 }}>
+    //         <AppBar position="static">
+    //             <Toolbar>
+    //             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+    //                 <div className='logo-container'>
+    //                     <img className="capco-logo" src={Logo} alt="logo"/>
+    //                     <span className="logo-text">Engineering</span>
+    //                 </div>
+    //             </Typography>
+    //                 <Button color="inherit" className="nav-links">Capabilities</Button>
+    //                 <Button color="inherit" className="nav-links">Team</Button>
+    //                 <Button color="inherit" className="nav-links">Career</Button>
+    //                 <Button color="inherit" className="nav-links">Articles</Button>
+    //                 <Button color="inherit" className="nav-links">Capco.com</Button>
+    //             </Toolbar>
+    //         </AppBar>
+    //         </Box>
+    //     </ThemeProvider>
+    // </Wrapper>
--- src/components/NavBar/styles/index.js
+++ src/components/NavBar/styles/index.js
@@ -1,37 +1,46 @@
 import styled from "styled-components/macro";
+import { Drawer } from "@mui/material";
 
-const Wrapper = styled.div`
+export const Wrapper = styled.div`
   .logo-container {
     display: grid;
-    grid-template-columns: 0fr 1fr;
+    grid-template-columns: repeat(2, min-content);
     grid-gap: 20px;
   }
 
-  .nav-links {
-      font-size: 12px;
-  }
-
   .capco-logo {
+    display: inline-block;
     width: 85px;
     height: 30.38px;
     left: calc(50% - 170px/2 - 715px);
   }
 
   .logo-text {
-    height: auto;
-    width: auto;
-    padding-top: -10px;
-    // padding-bottom: 23px;
-    
-  }
-  .capco-logo, .logo-text {
     display: inline-block;
+    font-family: Ubuntu Mono, Sans;
   }
 
-  .logo-text, .nav-links {
-      font-family: 
-      Ubuntu Mono;
+  .MuiButton-text {
+    font-family: Ubuntu Mono;
+    font-size: 18px;
+  }
+
+  .MuiPaper-root {
+    background-color: #000000;
   }
 `;
 
-export default Wrapper;
+export const StyledDrawer = styled(Drawer)`
+  .MuiDrawer-paper {
+    padding-top: 60px;
+    font-family: Ubuntu Mono;
+    opacity: 0.8;
+    text-transform: uppercase;
+    background-color: #000000;
+    color: #ffffff;
+  }
+  .MuiTypography-root {
+    font-family: Ubuntu Mono;
+    font-size: 22px;
+  }
+`
